f4be383ea37a811496a8111624f706dfc51e6e20
lucasdu57 <lucasdu573300@gmail.com>
2026-01-30 15:13:27 +0100

    script.js

--- script.js
+++ script.js
@@ -251,7 +251,7 @@ function logout() {
 /********************
  TIMER OUVERTURE
 ********************/
-const openDate = new Date("2026-07-05T13:00:00"); 
+const openDate = new Date("2026-02-05T13:00:00"); 
 
 function updateCountdown() {
     const now = new Date();
@@ -276,5 +276,6 @@ function updateCountdown() {
 if (localStorage.getItem("admin") !== "true") {
     setInterval(updateCountdown, 1000);
 }
+
 
 
